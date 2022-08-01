f0977af5a0b725c71dc8ee2c651f9da99a9dd09a
Serge Mouradian <serge.muradian@gmail.com>
2022-08-01 23:19:03 +0300

    fixed progressbar homework added

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,14 +2,14 @@ const form = document.getElementById("form");
 const progress = document.getElementById("progress");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
-    const file = document.getElementById("file").files;
-    const fileName = document.querySelector(".input__wrapper-desc");
-    const formData = new FormData();
-    formData.append(`${fileName.innerText}`, file[0]); request.upload.addEventListener("progress", function(e) {
-        progress.value = (e.loaded / e.total).toFixed(2);
-    }, false);
-    request.send(formData);
+  e.preventDefault();
+  const request = new XMLHttpRequest();
+  request.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
+  const formData = new FormData(form);
+
+  request.upload.onprogress = (e) => {
+      progress.value = (e.loaded) / (e.total);
+    };   
+
+  request.send(formData);
 });
